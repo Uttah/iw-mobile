b3edb24dfc242cae3a510394260859f9df468796
Natalia Dushkina <smirnovanatalia2008@gmail.com>
2018-08-08 09:41:03 +0300

    restore folder

---
+++ App/Components/Styles/TextFieldStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from 'App/Themes'
+
+export default StyleSheet.create({
+    errorLabel: {
+        color: Colors.error
+    },
+    icon: {
+        marginRight: 5
+    }
+});
