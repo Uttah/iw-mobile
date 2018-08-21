288519c2530d7bded4d12dfe31619e9a9351c0b8
Natalia Dushkina <smirnovanatalia2008@gmail.com>
2018-08-21 09:59:42 +0300

    codestyle: tabs fixes

--- App/Services/Validator.ts
+++ App/Services/Validator.ts
@@ -36,17 +36,17 @@ const validation = {
 };
 
 export default function validate(fieldName, value) {
-    var formValues = {};
-    formValues[fieldName] = value;
-  
-    var formFields = {};
-    formFields[fieldName] = validation[fieldName];
-  
-    const result = validatejs(formValues, formFields);
-  
-    if (result) {
-      return result[fieldName][0];
-    }
-  
-    return '';
+  var formValues = {};
+  formValues[fieldName] = value;
+
+  var formFields = {};
+  formFields[fieldName] = validation[fieldName];
+
+  const result = validatejs(formValues, formFields);
+
+  if (result) {
+    return result[fieldName][0];
+  }
+
+  return '';
 }
